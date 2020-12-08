5805d600b638438760a8421f156b6f56f9c1f04b
Paul Godard <paul.godard@outlook.fr>
2020-12-08 16:50:53 +0100

    log

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/Mongo-training', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/Mongo-training', {useNewUrlParser: true, useUnifiedTopology: true}).
+then(()=>{
+  console.log("Noicely connected")
+}).
+catch((error)=> {
+  console.log(error)
+})
